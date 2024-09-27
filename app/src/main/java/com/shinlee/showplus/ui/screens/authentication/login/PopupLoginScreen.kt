208c5c5859ddb8e9990be4932ab9d8af6da1281f
vyvienne <vyvienne@shinlee.ltd>
2024-09-27 16:50:38 +0700

    fix flow login

--- app/src/main/java/com/shinlee/showplus/ui/screens/authentication/login/PopupLoginScreen.kt
+++ app/src/main/java/com/shinlee/showplus/ui/screens/authentication/login/PopupLoginScreen.kt
@@ -4,6 +4,10 @@ import androidx.compose.material.AlertDialog
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.vector.ImageVector
 
 
@@ -15,34 +19,41 @@ fun AlertDialogExample(
     dialogText: String,
     icon: ImageVector,
 ) {
-    AlertDialog(
-        title = {
-            Text(text = dialogTitle)
-        },
-        text = {
-            Text(text = dialogText)
-        },
-        onDismissRequest = {
-            onDismissRequest()
-        },
-        confirmButton = {
-            TextButton(
-                onClick = {
-                    onConfirmation()
+    var showDialog by remember { mutableStateOf(true) }
+
+    if(showDialog) {
+        AlertDialog(
+            title = {
+                Text(text = dialogTitle)
+            },
+            text = {
+                Text(text = dialogText)
+            },
+            onDismissRequest = {
+                onDismissRequest()
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        onConfirmation()
+                    }
+                ) {
+                    Text("Confirm")
                 }
-            ) {
-                Text("Confirm")
-            }
-        },
-        dismissButton = {
-            TextButton(
-                onClick = {
-                    onDismissRequest()
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        onDismissRequest()
+                        showDialog = false
+                    }
+                ) {
+                    Text("Dismiss")
                 }
-            ) {
-                Text("Dismiss")
             }
-        }
-    )
+        )
+    }
+
+
 }
 
--- app/src/main/java/com/shinlee/showplus/ui/screens/profile/ProfileFragment.kt
+++ app/src/main/java/com/shinlee/showplus/ui/screens/profile/ProfileFragment.kt
@@ -25,27 +25,27 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.profile_fragment, container, false)
-        val composeView = view.findViewById<ComposeView>(R.id.compose_view)
-        composeView.apply {
-            // Dispose of the Composition when the view's LifecycleOwner
-            // is destroyed
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-            setContent {
-                // In Compose world
-                AlertDialogExample(
-                    onDismissRequest = { false },
-                    onConfirmation = {
-                        activity?.let{
-                            val intent = Intent (it, AuthenticationActivity::class.java)
-                            it.startActivity(intent)
-                        } // Add logic here to handle confirmation.
-                    },
-                    dialogTitle = "Alert dialog example",
-                    dialogText = "This is an example of an alert dialog with buttons.",
-                    icon = Icons.Default.Info
-                )
-            }
-        }
+//        val composeView = view.findViewById<ComposeView>(R.id.compose_view)
+//        composeView.apply {
+//            // Dispose of the Composition when the view's LifecycleOwner
+//            // is destroyed
+//            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+//            setContent {
+//                // In Compose world
+//                AlertDialogExample(
+//                    onDismissRequest = { false },
+//                    onConfirmation = {
+//                        activity?.let{
+//                            val intent = Intent (it, AuthenticationActivity::class.java)
+//                            it.startActivity(intent)
+//                        } // Add logic here to handle confirmation.
+//                    },
+//                    dialogTitle = "You have to login?",
+//                    dialogText = "Login to the app to continue to use this feature",
+//                    icon = Icons.Default.Info
+//                )
+//            }
+//        }
         return view
     }
 
