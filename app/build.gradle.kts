6e0e2a3924885a5f2fa1870ff2fee107a4923589
vyvienne <vyvienne@shinlee.ltd>
2024-10-18 10:59:17 +0700

    add new version code 24101801 and version name 0.5.4-alpha.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 24
         targetSdk = 34
 
-        versionCode = 24101101 // YYMMDDNN
-        versionName = "0.3.3-alpha.1"
+        versionCode = 24101801 // YYMMDDNN
+        versionName = "0.5.4-alpha.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
